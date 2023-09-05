4b84e1a1b691c216f689397ac98572d14595195e
momotsow <kgomotsomkansi@gmail.com>
2023-09-05 21:45:33 +0200

    Remove navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Banner } from './component/Banner/Banner';
 import AccordionSection from './component/Accordion/Accordion';
-import Header from './component/Header/navbar';
 
 function App() {
   return (
     <div className="">
-      <Header />
       <Routes>
         <Route path="/banner" element={<Banner />} />
         <Route path="/accordion" element={<AccordionSection />} />
