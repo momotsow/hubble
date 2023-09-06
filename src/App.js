115f7896087846758c457280084ebc443ca2e8ad
momotsow <kgomotsomkansi@gmail.com>
2023-09-06 02:19:43 +0200

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import Banner  from './component/Banner/Banner';
 import AccordionSection from './component/Accordion/Accordion';
 
-function App() {
-  return (
-    <div className="">
-      <Routes>
-        <Route path="/huble/banner" element={<Banner />} />
-        <Route path="/huble/accordion" element={<AccordionSection />} />
-      </Routes>
-    </div>
-  );
-}
+const App  = createBrowserRouter([ 
+  {
+    path: "/huble/banner",
+    element: <div><Banner /></div>,
+  },
+  {
+    path: "/huble/accordion",
+    element: <div><AccordionSection /></div>,
+  },
+]);
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Routers } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routers>
-      <App />
-    </Routers>
+  <RouterProvider router={ App } />
   </React.StrictMode>
 );
 
